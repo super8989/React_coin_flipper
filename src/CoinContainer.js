e5422ecf46147d53c0600a93e115b1f90c0860d5
Sam <super8989@gmail.com>
2019-10-03 23:02:52 -0400

    added in flip coin function to show heads or tails

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { choice } from './helpers';
 
 class CoinContainer extends Component {
     
@@ -16,14 +17,29 @@ class CoinContainer extends Component {
             nFlips: 0,
             nHeads: 0,
             nTails: 0
-        }
-        
+        };
+        this.handleClick = this.handleClick.bind(this);       
+    }
+
+    flipCoin() {
+        const newCoin = choice(this.props.coins);
+        this.setState(st => {
+            return {
+                currCoin: newCoin,
+                nFlips: st.nFlips + 1,
+            }
+        })
+    }
+
+    handleClick(e) {
+        this.flipCoin();
     }
 
     render() {
         return (
             <div className="CoinContainer">
                 <h2>Let's Flip a Coin!</h2>
+                <button onClick={this.handleClick}>Flip Me!</button>
                 <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
             </div>
         )
