c8ab3bc7293dbbed267eb8f5829a36348757f326
Sam <super8989@gmail.com>
2019-10-03 22:51:38 -0400

    setting up the skeleton for the coin flip exercise

---
+++ src/CoinContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class CoinContainer extends Component {
+    
+    static defaultProps = {
+        coins: [
+            { side: 'heads', imgSrc: 'https://tinyurl.com/react-coin-heads-jpg' },
+            { side: 'tails', imgSrc: 'https://tinyurl.com/react-coin-tails-jpg' }
+        ]
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        }
+        
+    }
+
+    render() {
+        return (
+            <div className="CoinContainer">
+                <h2>Let's Flip a Coin!</h2>
+                <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
+            </div>
+        )
+    }
+
+}
+
+export default CoinContainer;
